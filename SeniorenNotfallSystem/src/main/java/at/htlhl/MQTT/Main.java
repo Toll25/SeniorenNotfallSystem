6a859d8826778f49c4f4174b1d3d5b3fc140eb8d
Toll <paulipanther.hoesch@gmail.com>
2023-09-28 16:27:25 +0200

    psuh

--- SeniorenNotfallSystem/src/main/java/at/htlhl/MQTT/Main.java
+++ SeniorenNotfallSystem/src/main/java/at/htlhl/MQTT/Main.java
@@ -1,61 +1,93 @@
 package at.htlhl.MQTT;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.util.Arrays;
+
 public class Main {
 
+    static String serverURI = "tcp://eu1.cloud.thethings.network:1883"; // TTN MQTT server URI
+    static String clientId = "TestClientID"; // Unique client ID
+    static String username = "itp-project-1@ttn"; // TTN Application ID
+    static String password = "NNSXS.7FYSGXPXGD3KHCKUTGIDUSLNG7OMMY2D7LGIS6Y.SIDOXV3NJHDPX3SF5OMHRO52JFQE56JPRSKPB5N7HC6HVKE6P2QA"; // TTN Access Key
+    static String subTopic = "v3/itp-project-1@ttn/devices/uno-0004a30b001c1b03/up";
+    static String resTopic = "v3/itp-project-1@ttn/devices/uno-0004a30b001c1b03/down/push";
+    static MqttClient client;
 
+    static {
+        try {
+            client = new MqttClient(serverURI, clientId, new MemoryPersistence());
+        } catch (MqttException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) {
-        String broker = "tcp://eu1.cloud.thethings.network:1883";
-        MqttConnectOptions options = new MqttConnectOptions();
-        String username = "itp-project-1@ttn";
-        String password = "NNSXS.CCEKXFMR74QKCMM7ZRJ2SXCLVDTKFTZXYGMKNLY.NEZDSO3KPCTOZ6M2A3J6RPLL4O6J635HKMJFEZ7VUQDBUZMWLWNQ";
-        String clientid = "84847a26ff2d456aa422c99e348c9233";
-        String topic = "itp-project-1/message";
-        int qos = 0;
         try {
-            MqttClient client = new MqttClient(broker, clientid, new MemoryPersistence());
-            // connect options
+            MqttConnectOptions options = new MqttConnectOptions();
             options.setUserName(username);
             options.setPassword(password.toCharArray());
-            options.setConnectionTimeout(60);
-            options.setKeepAliveInterval(60);
-            // setup callback
-            client.setCallback(new MqttCallback() {
 
-                public void connectionLost(Throwable cause) {
-                    System.out.println("connectionLost: " + cause.getMessage());
-                }
-
-                public void messageArrived(String topic, MqttMessage message) {
-                    System.out.println("topic: " + topic);
-                    System.out.println("Qos: " + message.getQos());
-                    System.out.println("message content: " + new String(message.getPayload()));
-
-                }
-
-                public void deliveryComplete(IMqttDeliveryToken token) {
-                    System.out.println("deliveryComplete---------" + token.isComplete());
-                }
-
-            });
             client.connect(options);
-            client.subscribe(topic, qos);
-            client.subscribeWithResponse(topic,qos);
 
-            String content = "Hallo";
+            if (client.isConnected()) {
+                System.out.println("connected");
+            }
+            // Subscribe to a TTN topic (e.g., application's uplink topic)
+            client.subscribe(subTopic);
 
-            MqttMessage message = new MqttMessage(content.getBytes());
-            message.setQos(qos);
-            // publish message
-            client.publish(topic, message);
-            System.out.println("Message published");
-            System.out.println("topic: " + topic);
-            System.out.println("message content: " + content);
-        } catch (Exception e) {
+            // Define a callback for incoming messages
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    System.out.println("Connection lost: " + cause.getMessage());
+                    cause.printStackTrace();
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    System.out.println("Received message on topic: " + topic);
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    String json = new String((message.getPayload()));
+                    JsonNode jsonNode = objectMapper.readTree(json);
+                    //String response = );
+                    String response = (((jsonNode.get("uplink_message")).get("decoded_payload")).get("value")).toPrettyString();
+                    System.out.println("Message payload: " + response);
+
+                    if (response.equals("\"notfall\"")) {
+                        respond(0);
+                    } else if (response.equals("\"ok\"")) {
+                        respond(1);
+                    }
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    // Not used in this example
+                }
+            });
+
+            // Keep the program running to receive messages
+        } catch (MqttException e) {
+            System.err.println("MQTT Error Code: " + e.getReasonCode());
+            System.err.println("MQTT Error Message: " + e.getMessage());
             e.printStackTrace();
+        }
+    }
+
+    public static void respond(int code) {
+        MqttMessage message = new MqttMessage();
+
+        String payload = "{\n \"app\": \"senior\",\n \"type\": \"led\",\n\"value\": " + code + "\n}";
+        message.setPayload(payload.getBytes());
+        System.out.println(new String(message.getPayload()));
+        try {
+            client.publish(resTopic, message);
+        } catch (MqttException e) {
+            throw new RuntimeException(e);
         }
     }
 }
